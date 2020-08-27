6a8a4379ac2dc9196b7c3b49e8933e1df7da8a53
VictorMardones <v,mardones04@ufromail.cl>
2020-08-27 05:57:44 -0400

    Añadir botones básicos

--- src/main/java/guis/VentanaCalculadora.java
+++ src/main/java/guis/VentanaCalculadora.java
@@ -10,7 +10,32 @@ import javax.swing.*;
 public class VentanaCalculadora extends JFrame implements ActionListener{
 
     //// Atributos
+    /**
+     * Botón para realizar una suma
+     */
+    JButton sumarButton;
+    /**
+     * Botón para realizar una resta
+     */
+    JButton restarButton;
+    /**
+     * Botón para realizar una multiplicación
+     */
+    JButton multiplicarButton;
+    /**
+     * Botón para realizar una división
+     */
+    JButton dividirButton;
+    /**
+     * Botón para limpiar los números ingresados
+     */
+    JButton limpiarButton;
+    /**
+     * Botón para salir del programa
+     */
+    JButton salirButton;
 
+    //// Botones
 
     //// Métodos
     public void actionPerformed(ActionEvent ae){
